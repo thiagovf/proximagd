a8e94c4af3462cef702589d8aad4b55c1f4aa257
thiagovf <thiagu.vf@gmail.com>
2016-01-11 16:12:52 -0300

    adjusted to work with jpa

--- src/main/java/br/com/equipejr/controller/RootController.java
+++ src/main/java/br/com/equipejr/controller/RootController.java
@@ -1,17 +1,31 @@
 package br.com.equipejr.controller;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import br.com.equipejr.pojo.User;
+import br.com.equipejr.entity.User;
 
 @Controller
 public class RootController {
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String root() {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("equipePU");
+		User u = new User();
+		u.setName("yahoo");
+		u.setEmail("test");
+		EntityManager manager = factory.createEntityManager();
+		manager.getTransaction().begin();    
+		manager.persist(u);
+		manager.getTransaction().commit();  
+		factory.close();
+		manager.close();
 		return "snoop";
 	}
 	
@@ -20,9 +34,13 @@ public class RootController {
 			 @RequestParam(value="txtUserName", required=true) String email,
 			 @RequestParam(value="txtPass", required=true) String pass,
 			 ModelMap model) {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("tarefas");
+		EntityManager manager = factory.createEntityManager();
+
+		User encontrada = manager.find(User.class, 1L);
 		User user = new User();
 		user.setEmail(email);
-		model.addAttribute("message", "Hello, " + email);
+		model.addAttribute("message", "Hello, " + encontrada.getEmail());
 		model.addAttribute("user", user);
 		return "home";
 	}
---
+++ src/main/java/br/com/equipejr/entity/User.java
@@ -0,0 +1,35 @@
+package br.com.equipejr.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="user")
+public class User {
+
+	@Id
+	@GeneratedValue
+	private Long id;
+	
+	@Column(nullable = false)
+	private String name;
+	
+	@Column(nullable = false)
+	private String email;
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+}
